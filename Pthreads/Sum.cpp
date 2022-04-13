2cd723bfd7e4f7b659da83e5df457a2b4f0316e5
MarkGoncharovAl <goncharov.ma@phystech.edu>
2022-04-13 12:33:58 +0300

    Pthreads and Sort

---
+++ Pthreads/Sum.cpp
@@ -0,0 +1,98 @@
+// clang++-12 -std=c++2a -lpthread -lboost_program_options Sum.cpp 
+
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <pthread.h>
+
+namespace po = boost::program_options;
+
+namespace details {
+void
+check(int condition, size_t LINE, const char FILE[])
+{
+  if (condition) {
+    std::cerr << "Failed! Error code: " << condition;
+    std::abort();
+  }
+}
+}
+#define check(condition) details::check(condition, __LINE__, __FILE__)
+
+void*
+calcSum(void* ptr);
+
+// returns [threads, N]
+std::pair<int, int>
+processStart(int argc, char* argv[]);
+
+using pair = int[2];
+
+int
+main(int argc, char* argv[])
+{
+  auto [threadSize, N] = processStart(argc, argv);
+  int for_each = N / threadSize;
+  double sum = 0;
+  double offset = N % threadSize;
+
+  std::vector<pthread_t> threads(threadSize);
+  std::vector<std::unique_ptr<int[]>> sizes;
+  for (int i = 0; i < threadSize; ++i) {
+    sizes.push_back(std::unique_ptr<int[]>(new int[2]));
+    if (i == 0) {
+      sizes[i][0] = 0;
+      sizes[i][1] = for_each + offset;
+    } else {
+      sizes[i][0] = for_each * i + offset;
+      sizes[i][1] = sizes[i][0] + for_each;
+    }
+    check(pthread_create(&threads[i], nullptr, calcSum, (void*)sizes[i].get()));
+  }
+
+  for (auto& thread : threads) {
+    double* new_sum = nullptr;
+    check(pthread_join(thread, (void**)&new_sum));
+    sum += *new_sum;
+    delete new_sum;
+  }
+
+  std::cout << sum;
+  return 0;
+}
+
+void*
+calcSum(void* ptr)
+{
+  int* sizes = (int*)ptr;
+  double* sum = new double;
+  for (int i = sizes[0] + 1; i <= sizes[1]; ++i) {
+    *sum += 1.0 / (double)i;
+  }
+  pthread_exit((void*)sum);
+}
+
+// returns [threads, N]
+std::pair<int, int>
+processStart(int argc, char* argv[])
+{
+  po::options_description desc("Allowed options");
+  desc.add_options()("help,h", "Information about options")(
+    "threads,t", po::value<int>(), "Threads count")(
+    "N,n", po::value<int>(), "N to count untill");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc;
+    std::exit(0);
+  }
+
+  std::pair<int, int> out = { 1, 10 }; // default
+  if (vm.count("threads"))
+    out.first = vm["threads"].as<int>();
+  if (vm.count("N"))
+    out.second = vm["N"].as<int>();
+  return out;
+}
---
+++ Sort/main.cpp
@@ -0,0 +1,105 @@
+#include "def.h"
+#include <unistd.h>
+
+namespace fs = std::filesystem;
+constexpr std::string_view test_folder = "../tests";
+
+int main ()
+{
+    auto [size , rank] = mpi::init ();
+    if (size != 4)
+    {
+        std::cerr << "Must be 4 threads to process!\n";
+        quit ();
+    }
+
+    std::cout << "\n";
+    if (rank == mpi::rank_main)
+        mainWork ();
+    else
+        elseWork (rank);
+
+    quit ();
+}
+
+
+void mainWork ()
+{
+    auto streams = openFilesIn (std::string (test_folder));
+    for (auto& stream : streams)
+    {
+        size_t size = 0;
+        stream >> size;
+        std::vector<int> data (size);
+        for (auto& elem : data)
+            stream >> elem;
+        size_t extra_size = (4 - (data.size () % 4)) % 4;
+        std::vector<int> data2 (extra_size + data.size ());
+        for (auto& elem : data)
+            std::cout << elem << "\t";
+        std::cout << "\n";
+        std::copy (data.begin () , data.end () , data2.begin ());
+        for (auto iter = data2.rbegin (); iter != data2.rbegin () + extra_size; ++iter)
+            *iter = 100000;
+
+        auto start_time = MPI_Wtime ();
+        std::sort (data.begin () , data.end ());
+        auto end_time = MPI_Wtime ();
+        std::cout << "Std::sort:\t" << end_time - start_time << "\n";
+
+        start_time = MPI_Wtime ();
+        startWork (data2);
+        end_time = MPI_Wtime ();
+        std::cout << "My sort:\t" << end_time - start_time << "\n";
+    }
+}
+
+void startWork (std::vector<int>& data)
+{
+    int small_size = data.size () / 4;
+    for (int i = 1; i < 4; ++i)
+    {
+        mpi::send (small_size , i , 1);
+        mpi::send (data.data () + small_size * i , i , small_size);
+    }
+    std::vector<int> small_data (small_size);
+    std::copy (data.begin () , data.end () , small_data.begin ());
+    std::sort (small_data.begin () , small_data.end ());
+    mpi::send (small_data.data () , 2 , small_size);
+}
+
+void elseWork (int rank)
+{
+    auto size = mpi::recv<int> (0);
+    auto data = mpi::recvVector<int> (0 , size);
+    std::sort (data.begin () , data.end ());
+    if (rank / 2)
+    {
+        auto new_data = mpi::recvVector<int> (rank % 2 , size);
+        auto out = sortUnity(data, new_data);
+    }
+    else
+    {
+        mpi::send (data , 2 + rank % 2);
+    }
+}
+
+std::vector<std::ifstream> openFilesIn (const std::string& folder_name)
+{
+    std::vector<std::ifstream> out;
+    fs::path path { folder_name };
+    fs::directory_entry dir (path);
+    if (!dir.exists ())
+        mpi::quitAll ();
+
+    fs::directory_iterator iter (dir) , end;
+    for (; iter != end; ++iter)
+    {
+        if (fs::is_regular_file (*iter))
+        {
+            std::ifstream file (fs::path (*iter).string () , std::ios::in);
+            out.push_back (std::move (file));
+        }
+    }
+    return out;
+}
